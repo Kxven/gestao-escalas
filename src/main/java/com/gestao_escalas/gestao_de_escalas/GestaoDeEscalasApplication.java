a2fc9ee31bfd8f5c3754dee418946e91d1e02648
Kxven <kevengodinho3474@gmail.com>
2025-08-12 11:30:58 -0300

    Criação do projeto base com Spring Boot

---
+++ src/main/java/com/gestao_escalas/gestao_de_escalas/GestaoDeEscalasApplication.java
@@ -0,0 +1,13 @@
+package com.gestao_escalas.gestao_de_escalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeEscalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeEscalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao_escalas/gestao_de_escalas/GestaoDeEscalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_escalas.gestao_de_escalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeEscalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
